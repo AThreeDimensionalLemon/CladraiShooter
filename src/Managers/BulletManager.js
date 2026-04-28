2f136063404d4f9574562398b12d295b5e816d49
AThreeDimensionalLemon <eisigliang@gmail.com>
2026-04-28 16:22:40 -0700

    Removed invalid syntax

--- src/Managers/BulletManager.js
+++ src/Managers/BulletManager.js
@@ -4,11 +4,8 @@ class BulletManager extends Phaser.GameObjects.Group {
             name: "Bullets", 
             maxSize: 50
         });
-<<<<<<< HEAD
 
         scene.add.existing(this);
-=======
->>>>>>> 17c09797fc9fdbe23adaeb0c6eb5c15619cbf844
         return this;
     }
 }
