1f7b0bd7862a47ff174d882f5e04594a518778af
AThreeDimensionalLemon <eisigliang@gmail.com>
2026-04-28 15:27:34 -0700

    Added other textures to preload

--- src/Scenes/PlayField.js
+++ src/Scenes/PlayField.js
@@ -9,14 +9,29 @@ class PlayField extends Phaser.Scene {
 
     preload() {
         this.load.setPath("./assets");
+        this.load.image("Damage_1");
+        this.load.image("Damage_2");
+        this.load.image("Health_Hull");
+        this.load.image("Health_Shield");
+        this.load.image("Laser_Enemy");
+        this.load.image("Laser_Player");
+        this.load.image("Shield_1");
+        this.load.image("Shield_2");
+        this.load.image("Shield_3");
+        this.load.image("Ship_Artillerist");
+        this.load.image("Ship_Gunner");
+        this.load.image("Ship_Medic");
         this.load.image("Ship_Player");
+        this.load.image("Ship_Runner");
+        this.load.image("Ship_Wall");
     }
 
     create() {
         this.player = new Player(this);
+        this.bulletManager = new BulletManager();
     }
 
     update(time, delta) {
-        this.player.update(delta)
+        this.player.update(delta);
     }
 }
